2beb0ea9af887e7418e3e8616b1d6dabcd14de67
iamwalshy <iamwalshy@protonmail.com>
2023-03-28 03:12:31 -0400

    Fixing vercel errors and starting from the beginning yay

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './HomePage.scss'
 
 const HomePage = () => {
-  document.title = 'EdgePlus';
+  document.title = 'Tesla NIT Patna | Home';
   return (
     <div className='pt-16 '>
       <div className='justify-center flex items-center my-[8rem] flex-col text-5xl font-bold'>
